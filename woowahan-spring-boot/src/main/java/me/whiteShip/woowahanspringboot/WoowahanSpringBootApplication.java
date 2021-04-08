8e97f0e9ab2abe13cd888f5a4262490576efa858
VitoJeong <ckdghk987@gmail.com>
2021-04-08 23:50:18 +0900

    2021/04/08

---
+++ woowahan-spring-boot/src/main/java/me/whiteShip/woowahanspringboot/WoowahanSpringBootApplication.java
@@ -0,0 +1,13 @@
+package me.whiteShip.woowahanspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowahanSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoowahanSpringBootApplication.class, args);
+	}
+
+}
---
+++ woowahan-spring-boot/src/test/java/me/whiteShip/woowahanspringboot/WoowahanSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteShip.woowahanspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowahanSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
